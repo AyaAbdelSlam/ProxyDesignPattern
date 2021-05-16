71e763ca798099b592b54aa88f799d6921cc827b
Aya AbdulSlam <ahemaid@integrant.com>
2021-05-16 03:25:15 +0200

    Initial Create

---
+++ ProxyDesignPattern/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxyDesignPattern
+{
+    internal class Math : IMath
+
+    {
+        public double Add(double x, double y) { return x + y; }
+        public double Sub(double x, double y) { return x - y; }
+        public double Mul(double x, double y) { return x * y; }
+        public double Div(double x, double y) { return x / y; }
+    }
+
+}
